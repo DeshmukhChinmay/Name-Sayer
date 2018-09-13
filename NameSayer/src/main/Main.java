a82ee6df8e2f8b70699908f02f88db0aa7499817
Hong Shi <hongye1007@gmail.com>
2018-09-13 22:41:06 +1200

    Redo UI layout

--- NameSayer/src/main/Main.java
+++ NameSayer/src/main/Main.java
@@ -9,10 +9,9 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     private static Stage _primaryStage;
-    private static Stage _secondaryStage;
 
     private static Scene mainMenuScene;
-    private static Scene listMenuScene;
+    private static Scene listenMenuScene;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -20,14 +19,15 @@ public class Main extends Application {
 
         FXMLLoader mainMenuLoader = new FXMLLoader(getClass().getResource("../fxmlFiles/mainMenu.fxml"));
         Parent mainMenuPane = mainMenuLoader.load();
-        mainMenuScene = new Scene(mainMenuPane, 499, 206);
+        mainMenuScene = new Scene(mainMenuPane, 554, 394);
 
         FXMLLoader listLoader = new FXMLLoader(getClass().getResource("../fxmlFiles/listMenu.fxml"));
-        Parent listMenuPane = listLoader.load();
-        listMenuScene = new Scene(listMenuPane, 843, 487);
+        Parent listenMenuPane = listLoader.load();
+        listenMenuScene = new Scene(listenMenuPane, 635, 406);
 
         _primaryStage.setTitle("NameSayer");
         _primaryStage.setScene(mainMenuScene);
+        _primaryStage.setResizable(false);
         _primaryStage.show();
     }
     //Loads Main page in the primary stage
@@ -35,9 +35,10 @@ public class Main extends Application {
         _primaryStage.setScene(mainMenuScene);
     }
     //Loads list page in the primary stage
-    public static void loadListPage(){
-        _primaryStage.setScene(listMenuScene);
+    public static void loadListenPage(){
+        _primaryStage.setScene(listenMenuScene);
     }
+
     public static void main(String[] args) {
         launch(args);
     }
