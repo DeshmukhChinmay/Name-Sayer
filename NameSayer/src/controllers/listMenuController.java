2d4968b8659e6696365b3915e99b4922e0c997bf
Hong Shi <hongye1007@gmail.com>
2018-09-13 17:12:36 +1200

    MainMenu UI

---
+++ NameSayer/src/controllers/listMenuController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import main.Main;
+
+public class listMenuController {
+
+    //Returns to the main menu
+    public void backButtonPressed(){
+        Main.loadMainPage();
+    }
+}
