a33f2d8c89c59fdd217b6c6a2d66faeb924cde45
Hong Shi <hongye1007@gmail.com>
2018-09-14 23:17:15 +1200

    added play Scene

---
+++ NameSayer/src/controllers/PlayMenuController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import main.Main;
+
+public class PlayMenuController {
+
+        //Changes scene to where the list view of all creations are shown
+        public void backButtonPressed() {
+            Main.loadListPage();
+        }
+}
+
