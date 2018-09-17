2362ba4e38e1c1489902abf5349b812de81f5cb2
Hong Shi <hongye1007@gmail.com>
2018-09-17 15:57:10 +1200

    added quality button

--- NameSayer/src/controllers/PlayMenuController.java
+++ NameSayer/src/controllers/PlayMenuController.java
@@ -1,8 +1,12 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
 import main.Main;
 
 public class PlayMenuController {
+        @FXML
+        private ToggleButton qualityButton;
 
         //Changes scene to where the list view of all creations are shown
         public void backButtonPressed() {
@@ -13,5 +17,27 @@ public class PlayMenuController {
             Main.loadPracticePage();
 
         }
+//        public void getQualityRating(Names name){
+//            if (name.getBadQuality()){
+//                toggleButton.setSelected(true);
+//            }
+//            else{
+//                toggleButton.setSelected(false);
+//            }
+//
+//        }
+
+        public void qualityRatingSelected(){
+//            Gets the name of the currently playing name
+//            and sets the quality good or bad
+            if(qualityButton.isSelected()){
+                qualityButton.setText("Bad Quality");
+            }
+            else{
+                qualityButton.setText("Good Quality");
+            }
+
+        }
+
 }
 
